783863729978fd240f5343dac39a55848862380a
mohametalmeari <mohamet.almeari@gmail.com>
2024-03-04 05:51:34 +0300

    Create MobileSidebar component

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { MenuIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Sidebar } from "./sidebar";
+import { useEffect, useState } from "react";
+
+export const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <MenuIcon />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+};
