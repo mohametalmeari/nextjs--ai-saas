264da3367b9499807bed626a9ccc69ccc9b4cb09
mohametalmeari <mohamet.almeari@gmail.com>
2024-03-04 05:29:41 +0300

    Add routes to Sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,11 +1,64 @@
 "use client";
 
 import { cn } from "@/lib/utils";
+import {
+  CodeIcon,
+  ImageIcon,
+  LayoutDashboardIcon,
+  MessageSquareIcon,
+  MusicIcon,
+  SettingsIcon,
+  VideoIcon,
+} from "lucide-react";
 import { Montserrat } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
 
 const montserrat = Montserrat({ weight: "600", subsets: ["latin"] });
+
+const routes = [
+  {
+    label: "Dashboard",
+    icon: LayoutDashboardIcon,
+    href: "/dashboard",
+    color: "text-sky-500",
+  },
+  {
+    label: "Conversation",
+    icon: MessageSquareIcon,
+    href: "/conversation",
+    color: "text-violet-500",
+  },
+  {
+    label: "Image Generation",
+    icon: ImageIcon,
+    href: "/image",
+    color: "text-pink-700",
+  },
+  {
+    label: "Video Generation",
+    icon: VideoIcon,
+    href: "/video",
+    color: "text-orange-700",
+  },
+  {
+    label: "Music Generation",
+    icon: MusicIcon,
+    href: "/music",
+    color: "text-emerald-500",
+  },
+  {
+    label: "Code Generation",
+    icon: CodeIcon,
+    href: "/code",
+    color: "text-green-700",
+  },
+  {
+    label: "Settings",
+    icon: SettingsIcon,
+    href: "/settings",
+  },
+];
 
 export const Sidebar = () => {
   return (
@@ -19,7 +72,20 @@ export const Sidebar = () => {
             Genius
           </h1>
         </Link>
-        <div>Hi-Routes</div>
+        <div className="space-y-1">
+          {routes.map((route) => (
+            <Link
+              href={route.href}
+              key={route.href}
+              className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+            >
+              <div className="flex items-center flex-1">
+                <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
+                {route.label}
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
