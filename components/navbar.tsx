ca6ce5062138443ca079b773518d0a38a34c6e07
mohametalmeari <mohamet.almeari@gmail.com>
2024-03-04 04:46:37 +0300

    Create Navbar component

---
+++ components/navbar.tsx
@@ -0,0 +1,16 @@
+import { MenuIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import { UserButton } from "@clerk/nextjs";
+
+export const Navbar = () => {
+  return (
+    <div className="flex items-center p-4">
+      <Button variant="ghost" size="icon" className="md:hidden">
+        <MenuIcon />
+      </Button>
+      <div className="flex w-full justify-end">
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </div>
+  );
+};
