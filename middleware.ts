7ab325d5e1bde2eb4a69a7246566d76de4cac607
mohametalmeari <mohamet.almeari@gmail.com>
2024-03-04 04:33:15 +0300

    Add config object to middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -3,3 +3,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: ["/"], // Add unprotected routes here
 });
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
