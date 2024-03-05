4d60f988c9586ea10958b5f076fe87cab9ae08ff
mohametalmeari <mohamet.almeari@gmail.com>
2024-03-05 20:55:16 +0300

    Add API limit check and increase API limit

--- app/api/code/route.ts
+++ app/api/code/route.ts
@@ -1,3 +1,4 @@
+import { checkApiLimit, increaseApiLimit } from "@/lib/api-limit";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import OpenAI from "openai";
@@ -28,10 +29,18 @@ export async function POST(req: Request) {
       return new NextResponse("Messages are required", { status: 400 });
     }
 
+    const freeTrial = await checkApiLimit();
+
+    if (!freeTrial) {
+      return new NextResponse("Free trial has expired", { status: 403 });
+    }
+
     const res = await openai.chat.completions.create({
       messages: [instructionMessage, ...messages],
       model: "gpt-3.5-turbo",
     });
+
+    await increaseApiLimit();
 
     return NextResponse.json(res.choices[0].message);
   } catch (error) {
