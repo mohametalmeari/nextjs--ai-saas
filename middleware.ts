46b368975827da41994f8ec3e748848332709060
mohametalmeari <mohamet.almeari@gmail.com>
2024-03-06 22:41:02 +0300

    Create webhook route for handling Stripe events then: - Run: stripe login - Press Enter then Allow access - Run: stripe listen --forward-to localhost:3000/api/webhook - Copy and paste STRIPE_WEBHOOK_SECRET to .env

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/"], // Add unprotected routes here
+  publicRoutes: ["/", "/api/webhook"], // Add unprotected routes here
 });
 
 export const config = {
